fe335812367f970c17f132be711d9692961577c8
gogo <gogochristian009@gmail.com>
2025-03-08 15:35:57 +0100

    revu des locataires en impayés 4

--- app/Livewire/UnPaidLocators.php
+++ app/Livewire/UnPaidLocators.php
@@ -101,12 +101,11 @@ class UnPaidLocators extends Component
 
             } else {
                 ###___s'il n'a même pas de facture,
-                ##__cela revient qu'il est en impayé
                 ##__on verifie si on a depassé la date d'echeance
                 $echeance_date = strtotime(date("Y/m/d", strtotime($location->echeance_date)));
-                // $now = strtotime(date("Y/m/d", strtotime(now())));
+                $now = strtotime(date("Y/m/d", strtotime(now())));
 
-                if($echeance_date>now()){
+                if($echeance_date>$now){
                     array_push($locataires, $location);
                 }
             }
